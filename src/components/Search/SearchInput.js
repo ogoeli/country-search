4d72feb538b1659878460718f128e5a5f1536429
domcodesjs <domcodesjs@gmail.com>
2019-04-10 20:26:17 -0700

    need to work on filtering

--- src/components/Search/SearchInput.js
+++ src/components/Search/SearchInput.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchInput = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <input
       type='text'
       placeholder='Search for a country...'
       className='search__input'
+      onChange={e => setSearchTerm(e.target.value)}
     />
   );
 };
