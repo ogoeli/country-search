895b9b1b90951540c5c5a9d38c8380acb8ef9607
domcodesjs <domcodesjs@gmail.com>
2019-04-12 11:09:24 -0700

    region dropdown toggle working desktop

--- src/components/Search/SearchDropdown.js
+++ src/components/Search/SearchDropdown.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchDropdown = () => {
+  const [selectedRegion, setSelectedRegion] = useState('All Regions');
+  const [dropdown, setDropdown] = useState(false);
+  const regions = [
+    'All Regions',
+    'Africa',
+    'America',
+    'Asia',
+    'Europe',
+    'Oceania'
+  ];
+
+  const toggleDropDown = () => {
+    return setDropdown(!dropdown);
+  };
+
+  const displaySelectedRegion = e => {
+    setSelectedRegion(e.target.textContent);
+    return setDropdown(false);
+  };
+
+  const displayRegions = regions.filter(region => region !== selectedRegion);
+
   return (
     <div className='search__dropdown'>
-      <button className='search__dropdown__btn'>
-        Filter by Region <i className='fas fa-angle-down' />
+      <button className='search__dropdown__btn' onClick={toggleDropDown}>
+        {selectedRegion}{' '}
+        {dropdown ? (
+          <i className='fas fa-angle-up' />
+        ) : (
+          <i className='fas fa-angle-down' />
+        )}
       </button>
-
-      {/* <ul className='search__dropdown__list'>
-        <li>Africa</li>
-        <li>America</li>
-        <li>Asia</li>
-        <li>Europe</li>
-        <li>Oceania</li>
-      </ul> */}
+      {dropdown ? (
+        <ul className='search__dropdown__list'>
+          {displayRegions.map(region => (
+            <li key={region} onClick={displaySelectedRegion}>
+              {region}
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 };
